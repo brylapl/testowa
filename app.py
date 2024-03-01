d9684a91148ee8f805b6573ec9330a25d59ce1f6
brylapl <92885606+brylapl@users.noreply.github.com>
2024-03-01 17:48:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ import sqlite3
 from datetime import datetime
 
 # Tworzenie bazy danych SQLite
-conn = sqlite3.connect('errors.db')
+conn = sqlite3.connect('soccer.db')
 c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS errors 
              (id INTEGER PRIMARY KEY AUTOINCREMENT, 
