c8a8bd54204a671fdfa04efc4c03c4695824ac79
brylapl <92885606+brylapl@users.noreply.github.com>
2024-03-02 19:20:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,22 +15,17 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
-from tab1 import tab1
+
 
 st.write('APP')
 
 from tab1 import show_tab1
 from tab2 import show_tab2
 
-def main():
-    # Tworzenie zakładek
-    tabs = st.radio("Wybierz zakładkę:", ["Tab1", "Tab2"])
-    
-    if tabs == "Tab1":
-        show_tab1()
-    elif tabs == "Tab2":
-        show_tab2()
-if __name__ == "__main__":
-    main()
-    
+
+tabs = st.radio("Wybierz zakładkę:", ["Tab1", "Tab2"])
+
+if tabs == "Tab1":
+    show_tab1()
+elif tabs == "Tab2":
+    show_tab2()
