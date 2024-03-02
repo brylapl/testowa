7b67a4b85eb9af9fa625039ba9a35e80a668d280
brylapl <92885606+brylapl@users.noreply.github.com>
2024-03-02 10:42:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,5 +20,5 @@ from time import sleep
 st.write('APP')
 st.header('Tytul')
 
-st.markdown('<img src="pl.svg" alt="brak">',unsafe_allow_html=True)
+st.markdown('<img src="./pl.svg" alt="brak">',unsafe_allow_html=True)
 st.image("./pl.svg", caption='Tytuł zdjęcia')
