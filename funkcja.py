ed8a4a6a6799bd9ee950ee6e11a295c104029495
brylapl <92885606+brylapl@users.noreply.github.com>
2024-03-03 13:19:09 +0100

    Update funkcja.py

--- funkcja.py
+++ funkcja.py
@@ -6,6 +6,12 @@ def rodzaj(*args):
     th = f'''<tr>\n<th class='bg-dark text-white fw-bold'>{args[0]}</th>\n<th class='bg-dark text-white fw-bold'>{args[1]}</th>\n<th class='bg-dark text-white fw-bold'>{args[2]}</th>\n</tr>\n'''
     return th
 
+def dane(*kurs):
+    for i in range(len(kurs)):
+        t = f'''<tr>\n<td>{kurs[0][0]}</td>\n<td class='bg-dark text-white fw-bold'>{kurs[0][1]}</td>\n<td>{kurs[0][2]}</td>\n</tr>\n'''
+        return t
+
+
 def glowna():
     t = '<div class="container">\n'
     t += '<table class="table table-striped text-center table-dark">\n'
