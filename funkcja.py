5193e02ac343c1ac55de13f698641283a1055ac4
brylapl <92885606+brylapl@users.noreply.github.com>
2024-03-03 13:04:36 +0100

    Update funkcja.py

--- funkcja.py
+++ funkcja.py
@@ -2,7 +2,14 @@ def tytul(name):
 	th = f'''<tr>\n<th class='bg-dark text-white fw-bold h2' colspan="3">{name}</th>\n</tr>\n'''
 	return th
 
+def rodzaj(*args):
+    th = f'''<tr>\n<th class='bg-dark text-white fw-bold'>{args[0]}</th>\n<th class='bg-dark text-white fw-bold'>{args[1]}</th>\n<th class='bg-dark text-white fw-bold'>{args[2]}</th>\n</tr>\n'''
+    return th
 
 def glowna():
-    main = tytul('Regulaminowy czas')
-    return main
+    t = '<div class="container">\n'
+    t += '<table class="table table-striped text-center table-dark">\n'
+    t += '''<tr>\n<th class='bg-dark text-white fw-bold' colspan="3">Oczekiwane kursy</th>\n</tr>\n'''
+    t = tytul('Regulaminowy czas')
+    t += rodzaj('1','X','2')
+    return t
