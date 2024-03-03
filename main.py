638d13484dc6983e8196bce900c2b6d46bdacaed
brylapl <92885606+brylapl@users.noreply.github.com>
2024-03-03 12:56:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import tabele
+
+from tabele import *
 st.write('APP')
 
 st.write(tabele.pokaz())
