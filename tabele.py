8beb498b59a6b95bddfc6d8cdb1e9182deae0990
brylapl <92885606+brylapl@users.noreply.github.com>
2024-03-03 10:54:44 +0100

    Update tabele.py

--- tabele.py
+++ tabele.py
@@ -11,12 +11,15 @@ def sklady_wyjsciowe(list1,list2,flagi_home,flagi_away,tytul):
     elif len(list2) < max_len:
         list2 += [''] * (max_len - min_len)
         flagi_away += [''] * (max_len - min_len)
-        
+    
+    global nazwa    
     st.write(f'{len(list1)} {len(list2)}')
     st.write(len(flagi_home))
     html = "<div class='container mw-100'>\n"
     html += "<table class='table table-responsive table-striped text-center table-dark table-sm'>\n"
     html += f"<tr><th class='fw-bold' colspan='4'>{tytul}</th></tr>"
+    html += f"<tr><th class='fw-bold' colspan='4'>{nazwa}</th></tr>"
+    
     return html
 
 
