cd24bc43bf286fe78d67773eb50478dcc4452c1e
brylapl <92885606+brylapl@users.noreply.github.com>
2024-03-03 10:47:27 +0100

    Update tabele.py

--- tabele.py
+++ tabele.py
@@ -15,23 +15,6 @@ def sklady_wyjsciowe(list1,list2,flagi_home,flagi_away,tytul):
     st.write(f'{len(list1)} {len(list2)}')
     st.write(len(flagi_home))
     
-    # tworzenie tabeli HTML
-    html = "<div class='container mw-100'>\n"
-    html += "<table class='table table-responsive table-striped text-center table-dark table-sm'>\n"
-    html += f"<tr><th class='fw-bold' colspan='4'>{tytul}</th></tr>"
-    html += "<tr>\n"
-    html += f"<th class='bg-dark text-white fw-bold' colspan='2'>HOME NAME</th>\n"
-    html += f"<th class='bg-dark text-white fw-bold' colspan='2'>AWAY NAME}</th>\n"
-    html += "</tr>\n"
-    for i in range(max_len):
-        html += "<tr>\n"
-        html += f"<td><img src='{flagi_home[i]}' alt=''</img></td><td>{list1[i]}</td>\n"
-
-        html += f"<td><img src='{flagi_away[i]}' alt=''</img><td>{list2[i]}</td>\n"
-        html += "</tr>\n"
-
-    html += "</table>\n"
-    html += "</div>"
 
 
     
