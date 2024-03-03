6050be4642df84b657ca2c643cbff1296bcaf11b
brylapl <92885606+brylapl@users.noreply.github.com>
2024-03-03 10:43:00 +0100

    Update tabele.py

--- tabele.py
+++ tabele.py
@@ -1,13 +1,17 @@
 import streamlit as st
 
-def sklady_wyjsciowe(list1,list2):
+def sklady_wyjsciowe(list1,list2,flagi_home,flagi_away):
     max_len = max(len(list1), len(list2))
     min_len = min(len(list1), len(list2))
     st.write(max_len, 'vs' ,min_len)
     
     if len(list1) < max_len:
         list1 += [''] * (max_len - min_len)
+        flagi_home += [''] * (max_len - min_len)
     elif len(list2) < max_len:
         list2 += [''] * (max_len - min_len)
+        flagi_away += [''] * (max_len - min_len)
+        
     st.write(f'{len(list1)} {len(list2)}')
+    st.write(len(flagi_home))
     
