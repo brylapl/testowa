9f039c41e63c12e8f1fa76aa93a8cb9b4eb36433
brylapl <92885606+brylapl@users.noreply.github.com>
2024-03-03 11:05:21 +0100

    Update tabele.py

--- tabele.py
+++ tabele.py
@@ -1,26 +1,2 @@
-import streamlit as st
-
-def sklady_wyjsciowe(list1,list2,flagi_home,flagi_away,tytul,druzyna):
-    global druzyna 
-    max_len = max(len(list1), len(list2))
-    min_len = min(len(list1), len(list2))
-    st.write(max_len, 'vs' ,min_len)
-    
-    if len(list1) < max_len:
-        list1 += [''] * (max_len - min_len)
-        flagi_home += [''] * (max_len - min_len)
-    elif len(list2) < max_len:
-        list2 += [''] * (max_len - min_len)
-        flagi_away += [''] * (max_len - min_len)
-    
-       
-    st.write(f'{len(list1)} {len(list2)}')
-    st.write(len(flagi_home))
-    html = "<div class='container mw-100'>\n"
-    html += "<table class='table table-responsive table-striped text-center table-dark table-sm'>\n"
-    html += f"<tr><th class='fw-bold' colspan='4'>{tytul}</th></tr>"
-    st.write(druzyna)
-    return html
-
-
-    
+def zmienna():
+    return 1_000_000
