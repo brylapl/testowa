76cbb63a9f26c1bdaa22114c152cbde8c2b9db32
brylapl <92885606+brylapl@users.noreply.github.com>
2024-03-03 11:35:33 +0100

    Update tabele.py

--- tabele.py
+++ tabele.py
@@ -1,7 +1,23 @@
-zmienna = 500
+def html(list1, list2,tytul):
+    max_len = max(len(list1), len(list2))
+    min_len = min(len(list1), len(list2))
 
-def pokaz():
-    global name
-    x = f'nazwa to'
-    g = globals()
-    return g
+    # dodanie pustych wartości do krótszej listy
+    if len(list1) < max_len:
+        list1 += [''] * (max_len - min_len)
+        flagi_home += [''] * (max_len - min_len)
+
+
+    elif len(list2) < max_len:
+        list2 += [''] * (max_len - min_len)
+        flagi_away += [''] * (max_len - min_len)
+
+
+    # tworzenie tabeli HTML
+    html = "<div class='container mw-100'>\n"
+    html += "<table class='table table-responsive table-striped text-center table-dark table-sm'>\n"
+    html += f"<tr><th class='fw-bold' colspan='4'>{tytul}</th></tr>"
+    html += "<tr>\n"
+    html += f"<th class='bg-dark text-white fw-bold' colspan='2'>{soccerrating_home_name}</th>\n"
+    html += f"<th class='bg-dark text-white fw-bold' colspan='2'>{soccerrating_away_name}</th>\n"
+    return html
