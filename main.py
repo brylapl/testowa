fc5da5c81c89eba229fdd5f08893ade6a2f2ea47
brylapl <92885606+brylapl@users.noreply.github.com>
2024-03-03 10:27:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,16 +15,19 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from tab1 import show_tab1
-from tab2 import show_tab2
+from tabele import sklady_wyjsciowe
 
 st.write('APP')
 
 tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
 
 with tab1:
-    show_tab1()
+    st.write('STRONA 1')
    
 with tab2:
-    show_tab2()
+    st.header('Jakias druga')
+
+with tab3:
+    st.write('WYWOŁANA FUNKCJĄ ZEWNĘTRZNĄ')
+    
 
