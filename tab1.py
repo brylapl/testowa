ed9c4efc74a0fbaa99b36008d113001ee30e39dc
brylapl <92885606+brylapl@users.noreply.github.com>
2024-03-03 10:27:12 +0100

    Update and rename tab1.py to tabele.py

--- tab1.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-def show_tab1():
-    st.title("Zakładka 1")
-    st.write("To jest zawartość zakładki 1")
---
+++ tabele.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+def sklady_wyjsciowe():
+    st.write('SKLADY')
