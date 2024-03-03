1917b5be57fefd2d295a4b22a287f2bde002da1d
brylapl <92885606+brylapl@users.noreply.github.com>
2024-03-03 14:34:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,22 +19,6 @@ import funkcja
 
 #------------------------ TABELA NA KURSY
 		
-
-win = [1,2,3]
-dc = [1,2,3]
-gole0 = [1,'0.5',2]
-gole1 = [1,'1.5',2]
-gole2 = [1,'2.5',2]
-gole3 = [1,'3.5',2]
-gole4 = [1,'4.5',2]
-dnb = [1,'AH0',2]
-bts = [1,'',2]
-home0 = [1,'0.5',2]
-home1 = [1,'1.5',2]
-away0 = [1,'0.5',2]
-away1 = [1,'1.5',2]
-		
-
 x = funkcja.pokaz()
 st.write(x)
 
