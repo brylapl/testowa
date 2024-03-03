d0468b59a4c2969155c55a36b9b51b4cede83ce8
brylapl <92885606+brylapl@users.noreply.github.com>
2024-03-03 12:57:42 +0100

    Update funkcja.py

--- funkcja.py
+++ funkcja.py
@@ -1,14 +1,3 @@
-def A(imie):
-    return f'Imie to {imie}'
-
-def B(nazwisko):
-    return f'Nazwisko to {nazwisko}'
-
-def C(kraj):
-    return f'Kraj to {kraj}'
-
-def pokaz():
-    a = A('Tom')
-    b = B('H')
-    c = C('Poland)
-    return f'Calosc to {a}-{b}-{c}'
+def pierwsza(imie):
+    return imie
+    
