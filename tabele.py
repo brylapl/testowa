c4d82e028e8e47f903fbab40c8aed8f44a7ced58
brylapl <92885606+brylapl@users.noreply.github.com>
2024-03-03 10:48:25 +0100

    Update tabele.py

--- tabele.py
+++ tabele.py
@@ -15,7 +15,8 @@ def sklady_wyjsciowe(list1,list2,flagi_home,flagi_away,tytul):
     st.write(f'{len(list1)} {len(list2)}')
     st.write(len(flagi_home))
     html = "<div class='container mw-100'>\n"
-    
+    html += "<table class='table table-responsive table-striped text-center table-dark table-sm'>\n"
+    html += f"<tr><th class='fw-bold' colspan='4'>{tytul}</th></tr>"
 
 
     
