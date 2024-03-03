45fdbdbf3dc03a1b2bb62bbc376cb862b88d9db3
brylapl <92885606+brylapl@users.noreply.github.com>
2024-03-03 11:01:57 +0100

    Update tabele.py

--- tabele.py
+++ tabele.py
@@ -1,5 +1,5 @@
 import streamlit as st
-druzyna = None
+druzyna = 'test'
 def sklady_wyjsciowe(list1,list2,flagi_home,flagi_away,tytul):
     global druzyna 
     max_len = max(len(list1), len(list2))
