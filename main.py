7fd5b70cb2574f7480dd52143e12e7e07f8246e1
brylapl <92885606+brylapl@users.noreply.github.com>
2024-03-03 11:05:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,28 +15,9 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import tabele
+from tabele import zmienna
 
 st.write('APP')
 
-lista1 = [1,2,3,4,5]
-lista2 = [10,11,12]
-flagi_home = ['f1','f2','f3','f4','f5']
-flagi_away = ['f10','f11','f12']
-druzyna = 'GLOBALNA NAZWA'
-tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
 
-with tab1:
-    st.write('STRONA 1')
-   
-with tab2:
-    st.header('Jakias druga')
-
-with tab3:
-    soccerrating_home_name = 'AS MONACO'
-    st.write('WYWOŁANA FUNKCJĄ ZEWNĘTRZNĄ')
-    
-    x = tabele.sklady_wyjsciowe(lista1,lista2,flagi_home,flagi_away,'TITLE',druzyna)
-    st.write(x)
-    
 
