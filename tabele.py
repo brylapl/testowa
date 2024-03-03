2017b58d4fb38d12b643efeaccf3f7401d160d83
brylapl <92885606+brylapl@users.noreply.github.com>
2024-03-03 10:38:55 +0100

    Update tabele.py

--- tabele.py
+++ tabele.py
@@ -1,15 +1,13 @@
 import streamlit as st
 
 def sklady_wyjsciowe(list1,list2):
-max_len = max(len(list1), len(list2))
-min_len = min(len(list1), len(list2))
-st.write(max_len, 'vs' ,min_len)
-# dodanie pustych wartości do krótszej listy
-if len(list1) < max_len:
-    list1 += [''] * (max_len - min_len)
-
-elif len(list2) < max_len:
-    list2 += [''] * (max_len - min_len)
-  
-st.write(max_len, 'vs' ,min_len)
+    max_len = max(len(list1), len(list2))
+    min_len = min(len(list1), len(list2))
+    st.write(max_len, 'vs' ,min_len)
+    
+    if len(list1) < max_len:
+        list1 += [''] * (max_len - min_len)
+    elif len(list2) < max_len:
+        list2 += [''] * (max_len - min_len)
+    st.write(max_len, 'vs' ,min_len)
     
