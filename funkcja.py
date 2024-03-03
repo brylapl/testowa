63995b308e46f1916f9773d14326fe12c166284c
brylapl <92885606+brylapl@users.noreply.github.com>
2024-03-03 13:03:09 +0100

    Update funkcja.py

--- funkcja.py
+++ funkcja.py
@@ -1,7 +1,8 @@
-def pierwsza(imie):
-    return imie
+def tytul(name):
+	th = f'''<tr>\n<th class='bg-dark text-white fw-bold h2' colspan="3">{name}</th>\n</tr>\n'''
+	return th
 
 
 def glowna():
-    main = pierwsza('Hub')
+    main = tytul('Regulaminowy czas')
     return main
