ad1fb8721bca4027c2a62aca8d57e73811039074
brylapl <92885606+brylapl@users.noreply.github.com>
2024-03-04 19:33:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,9 +18,24 @@ from selenium.webdriver.support import expected_conditions as EC
 import funkcja
 
 #------------------------ TABELA NA KURSY
-		
-x = funkcja.pokaz()
-st.write(x)
+from weasyprint import HTML
+
+# Tworzenie formularza w aplikacji Streamlit
+st.markdown("# Wprowadź dane:")
+name = st.text_input("Wprowadź swoje imię")
+age = st.text_input("Wprowadź swój wiek")
+
+# Utworzenie zawartości do zapisania do pliku PDF
+html = f"<h1>{name}</h1>"
+html += f"<p>Wiek: {age}</p>"
+
+# Zapisanie zawartości do pliku PDF
+pdf_file = f"{name}_info.pdf"
+HTML(string=html).write_pdf(pdf_file)
+
+# Wyświetlenie linku do pobrania pliku PDF
+st.markdown(f"[Pobierz plik PDF z informacjami]({pdf_file})")
+
 
 
 
