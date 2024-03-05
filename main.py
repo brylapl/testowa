47c9e55eb8e5dafa4ef5fe6a5cb4a2a0c04742c6
brylapl <92885606+brylapl@users.noreply.github.com>
2024-03-05 14:16:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
-from git import Repo
 #-------------------------------------------------------------------------------------------
 
 headers = requests.utils.default_headers()
