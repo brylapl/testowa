0c7bd6f0cb5c9abc54fe139b52f5fe23befda4fd
brylapl <92885606+brylapl@users.noreply.github.com>
2024-03-05 21:03:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 #-------------------------------------------------------------------------------------------
+import smtplib
+import schedule
+import time
 
 headers = requests.utils.default_headers()
 headers.update({
@@ -26,3 +29,28 @@ headers.update({
 st.set_page_config(page_title='Testowe', page_icon=":soccer:", layout="centered", initial_sidebar_state="collapsed", menu_items=None)
 st.header('baza danych')
 
+
+
+# Funkcja do wysyłania maila
+def send_email():
+    st.text("Sending email...")
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login("your_email@gmail.com", "your_password")
+    server.sendmail("your_email@gmail.com", "recipient_email@gmail.com", "This is a test email")
+    server.quit()
+    st.text("Email sent successfully")
+
+# Funkcja do planowania wysyłania maila o ustalonej godzinie
+def schedule_email():
+    schedule.every().day.at("12:00").do(send_email)  # Ustawienie godziny wysyłki maila
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+# Wywołanie funkcji planowania wysyłania maila
+schedule_email()
+
+
+st.write("DB username:", st.secrets["LOGIN"])
+st.write("DB password:", st.secrets["PASSWORD"])
