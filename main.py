64448aa3f90c4e0795379a50e1b522b60e1b0f66
brylapl <92885606+brylapl@users.noreply.github.com>
2024-04-19 13:46:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -32,5 +32,5 @@ def on_change(key):
     
 selected5 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'],
                         icons=['house', 'cloud-upload', "list-task", 'gear'],
-                        on_change=on_change, key='menu_5', orientation="horizontal")
+                        on_change=on_change, key='menu_5', orientation="vertical")
 selected5
