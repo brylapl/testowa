2032046075a784bb9440dfc01952e2c3b981a6d7
brylapl <92885606+brylapl@users.noreply.github.com>
2024-04-19 13:19:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import mysql.connector
 #-------------------------------------------------------------------------------------------
+from streamlit_option_menu import option_menu
 headers = requests.utils.default_headers()
 headers.update({
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
@@ -25,30 +26,51 @@ headers.update({
 
 st.set_page_config(page_title='Testowe', page_icon=":soccer:", layout="centered", initial_sidebar_state="collapsed", menu_items=None)
 st.header('BAZA DANYCH')
-try:
-    conn = mysql.connector.connect(host=st.secrets["host"], user=st.secrets["user"], password=st.secrets["password"], database=st.secrets["database"] )
-    c = conn.cursor()
-    st.write("Połączenie z bazą danych zostało nawiązane")
-except:
-    st.write(f"Błąd połączenia z bazą danych")
 
-# Formularz zgłaszania błędów i propozycji zmian
-st.title("Formularz zgłaszania błędów i propozycji zmian")
 
-category = st.selectbox("Wybierz kategorię zgłoszenia:", ["Error", "Inne"])
-message = st.text_area("Wiadomość")
-email = st.text_input("Email kontaktowy")
+# 1. as sidebar menu
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Home", 'Settings'], 
+        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+    selected
 
-now = datetime.now()
-date_time = now.strftime("%Y-%m-%d")
-time = now.strftime("%H:%M:%S")
+# 2. horizontal menu
+selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
+    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    menu_icon="cast", default_index=0, orientation="horizontal")
+selected2
 
-if st.checkbox("Submit"):
-    # Wstawienie danych do bazy danych
-    sql = "INSERT INTO feedback (category, message, email, date_time, time) VALUES (%s, %s, %s, %s, %s)"
-    val = (category, message, email, date_time, time)
-    c.execute(sql, val)
-    conn.commit()
-    st.success("Zgłoszenie zostało wysłane pomyślnie!")
+# 3. CSS style definitions
+selected3 = option_menu(None, ["Home", "Upload",  "Tasks", 'Settings'], 
+    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    menu_icon="cast", default_index=0, orientation="horizontal",
+    styles={
+        "container": {"padding": "0!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "green"},
+    }
+)
 
-conn.close()
+# 4. Manual item selection
+if st.session_state.get('switch_button', False):
+    st.session_state['menu_option'] = (st.session_state.get('menu_option', 0) + 1) % 4
+    manual_select = st.session_state['menu_option']
+else:
+    manual_select = None
+    
+selected4 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
+    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    orientation="horizontal", manual_select=manual_select, key='menu_4')
+st.button(f"Move to Next {st.session_state.get('menu_option', 1)}", key='switch_button')
+selected4
+
+# 5. Add on_change callback
+def on_change(key):
+    selection = st.session_state[key]
+    st.write(f"Selection changed to {selection}")
+    
+selected5 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'],
+                        icons=['house', 'cloud-upload', "list-task", 'gear'],
+                        on_change=on_change, key='menu_5', orientation="horizontal")
+selected5
