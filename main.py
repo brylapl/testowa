24128e94b7c1d03e87d20902bed44f481835802e
brylapl <92885606+brylapl@users.noreply.github.com>
2024-04-19 09:36:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,5 +24,6 @@ headers.update({
 
 st.set_page_config(page_title='Testowe', page_icon=":soccer:", layout="centered", initial_sidebar_state="collapsed", menu_items=None)
 st.header('BAZA DANYCH')
+st.write("DB username:", st.secrets["user"])
 
 
