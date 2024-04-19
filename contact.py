a61e0684c4d7f97b6ceeeda0f78e9723be6188a1
brylapl <92885606+brylapl@users.noreply.github.com>
2024-04-19 14:07:25 +0200

    Update contact.py

--- contact.py
+++ contact.py
@@ -1,12 +1,14 @@
 import streamlit as st
 
-# Formularz zgłaszania błędów i propozycji zmian
-st.title("Formularz zgłaszania błędów i propozycji zmian")
 
-category = st.selectbox("Wybierz kategorię zgłoszenia:", ["Error", "New"])
-
-message = st.text_area("Wiadomość")
-email = st.text_input("Email kontaktowy")
-
-if st.button("Wyślij"):
-    st.success("Zgłoszenie zostało wysłane pomyślnie!")
+def contact():
+    # Formularz zgłaszania błędów i propozycji zmian
+    st.title("Formularz zgłaszania błędów i propozycji zmian")
+    
+    category = st.selectbox("Wybierz kategorię zgłoszenia:", ["Error", "New"])
+    
+    message = st.text_area("Wiadomość")
+    email = st.text_input("Email kontaktowy")
+    
+    if st.button("Wyślij"):
+        st.success("Zgłoszenie zostało wysłane pomyślnie!")
