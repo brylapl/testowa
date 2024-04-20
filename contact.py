dd97281c0df8c2dcf0e869ff49fc59f7abe7a0e8
brylapl <92885606+brylapl@users.noreply.github.com>
2024-04-20 11:42:00 +0200

    Update contact.py

--- contact.py
+++ contact.py
@@ -1,11 +1,9 @@
 import streamlit as st
 
-def contact():
-    st.markdown("<h1 style='text-align: center;'>Formularz kontaktowy</h1>", unsafe_allow_html=True)
-    st.write('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"> ', unsafe_allow_html=True)
-   
-    with st.form(key='contact_form', clear_on_submit=True):
-        st.text_input('Imię', key='name')
-        st.text_input('Email', key='email')
-        st.text_area('Wiadomość', key='message', height=200)
-    st.form_submit_button('Wyślij', class_='btn btn-primary')
+st.title('Formularz kontaktowy')
+wiadomosc = st.text_area('Wiadomość')
+email = st.text_input('Email (opcjonalnie)')
+kategoria = st.selectbox('Kategoria', ['błąd', 'new'])
+
+if st.button('Wyślij'):
+    st.success('Wiadomość została wysłana.')
