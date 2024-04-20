d89cdfe84406c98b1872c52501e58ee6dd8c7bea
brylapl <92885606+brylapl@users.noreply.github.com>
2024-04-20 13:35:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -54,6 +54,7 @@ with open('style.css') as f:
 #         about_content = file.read()
 #         st.markdown(about_content, unsafe_allow_html=True)
 
+btn = st.button('Uruchom')
 st.markdown('''
 <!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 <title>Landing Page</title> <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"> 
