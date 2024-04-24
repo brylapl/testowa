a330fb4363948b7935fabadc3b7570282e16e7ea
brylapl <92885606+brylapl@users.noreply.github.com>
2024-04-24 20:05:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -38,8 +38,18 @@ with open("html/index.html", "r") as file:
 #------------------------------------------------------------------------------------------------
 
 st.markdown("""
-<body>
-    <div class="form-check form-switch pt-4">
+<!doctype html>
+<html lang="en" data-bs-theme="light">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <div class="form-check form-switch pt-5">
     <input class="form-check-input" type="checkbox" id="darkModeSwitch">
     <label class="form-check-label" for="darkModeSwitch">Dark Mode</label>
   </div>
@@ -57,6 +67,7 @@ st.markdown("""
     });
   </script>
   </body>
+</html>
 """,unsafe_allow_html=True)
 
 st.markdown("""
