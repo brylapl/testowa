c875e7dd049e526f11ebf5ebdd42b0afd46128cd
brylapl <92885606+brylapl@users.noreply.github.com>
2024-04-24 17:48:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -32,9 +32,9 @@ with open('html/style.css') as f:
     st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
 #------------------------------------------------------------------------------------------------
 #HTML
-with open("html/index.html", "r") as file:
-    index = file.read()
-    st.markdown(index, unsafe_allow_html=True)
+# with open("html/index.html", "r") as file:
+#     index = file.read()
+#     st.markdown(index, unsafe_allow_html=True)
 #------------------------------------------------------------------------------------------------
 
 genre = st.radio(
@@ -42,23 +42,49 @@ genre = st.radio(
     ["[Comedy]", "Drama", "Documentary"],
     captions = ["Laugh out loud.", "Get the popcorn.", "Never stop learning."])
 
-if genre == '[Comedy]':
-    st.write('You selected comedy.')
-else:
-    st.write("You didn\'t select comedy.")
-
-import streamlit as st
 
 st.markdown("### Radio buttons with Streamlit")
 
 option = st.radio("Select an option:", ["Option 1", "Option 2", "Option 3"])
 
 html_code = """
-<div class="row">
-  <div class="col">Column 1</div>
-  <div class="col">Selected option: {}</div>
-  <div class="col">Column 3</div>
+<!doctype html>
+<html lang="en" data-bs-theme="dark">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <div class="container-fluid p-5 bg-primary text-white text-center">
+  <h1>My First Bootstrap Page</h1>
+  <p>Resize this responsive page to see the effect!</p> 
 </div>
+  
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Kolumna 1</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Kolumna 2</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>{}</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Kolumna 3</h3>        
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+  </div>
+</div>
+  </body>
+</html>
 """.format(option)
 
 st.markdown(html_code, unsafe_allow_html=True)
