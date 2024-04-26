cd73d51fb49b346434df777ae1a9ba61eb353c7f
brylapl <92885606+brylapl@users.noreply.github.com>
2024-04-26 17:12:47 +0200

    Update contact.py

--- contact.py
+++ contact.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 def contact():
-    st.title('Formularz kontaktowy')
     kategoria = st.selectbox('Kategoria', ['Błąd','Nowe funkcje','Inne'],help="Wybierz kategorię wiadomości w jakiej chcesz się skontaktować.")
     wiadomosc = st.text_area('Wiadomość')
     email = st.text_input('Email (opcjonalnie)', help="Podaj swój e-mail jeżeli Twoja wiadomość wymaga odpowiedzi.")
