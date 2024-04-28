be46f1f389035ea66595af2215c36e0e5c6e20f8
brylapl <92885606+brylapl@users.noreply.github.com>
2024-04-28 14:47:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,87 +22,311 @@ from contact import contact
 from streamlit_option_menu import option_menu
 headers = requests.utils.default_headers()
 headers.update({
-    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
+'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
 })
 
 st.set_page_config(page_title='Testowe', page_icon=":soccer:", layout="wide", initial_sidebar_state="collapsed", menu_items=None)
 #------------------------------------------------------------------------------------------------
 #STYLE CSS
 with open('style.css') as f:
-    st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
+st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
 #------------------------------------------------------------------------------------------------
 #HTML
 #with open("html/index.html", "r") as file:
-    #index = file.read()
-    #st.markdown(index, unsafe_allow_html=True)
+#index = file.read()
+#st.markdown(index, unsafe_allow_html=True)
 #------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------
 #CONTACT
 #with open("html/contacts.html", "r") as file:
-    #contacts = file.read()
-    #st.markdown(contacts, unsafe_allow_html=True)
+#contacts = file.read()
+#st.markdown(contacts, unsafe_allow_html=True)
 #------------------------------------------------------------------------------------------------
 #contact()
 #------------------------------------------------------------------------------------------------
-st.markdown("""<!DOCTYPE html>
+st.markdown("""
+<!DOCTYPE html>
 <html lang="en">
-
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-<title>bootstrap</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Mobile App Screen</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-<link rel="stylesheet" href="/Features-Cards-icons.css">
-<link rel="stylesheet" href="/style.css">
-</head>
+<style> 
+.menu { 
+display: flex; 
+justify-content: center; 
+list-style: none; 
+padding: 0; } 
+.menu li { 
+margin: 0 10px; 
+} 
+.content { 
+padding: 20px; 
+} 
+@media only screen and (min-width: 768px) { 
+.navbar { 
+background-color: #007bff; 
+color: white; 
+padding: 10px 0;
+justify-content: space-evenly;
+} 
+.nav-link { 
+color: white !important; 
+padding: 0 10px !important; 
+} 
+} 
+@media only screen and (max-width: 767px) { 
+.navbar { 
+position: fixed; 
+bottom: 0; 
+width: 100%; 
+background-color: #007bff; 
+color: white; 
+display: flex; 
+justify-content: space-evenly; 
+padding: 10px 0; 
+} 
+.nav-link { 
+color: white !important; 
+padding: 0 10px !important; 
+} 
+} 
+</style>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
 
 <body>
-<div class="container py-4 py-xl-5">
-<div class="row mb-5">
-<div class="col-md-8 col-xl-6 text-center mx-auto">
-<h2>Heading</h2>
-<p class="w-lg-50">Curae hendrerit donec commodo hendrerit egestas tempus, turpis facilisis nostra nunc. Vestibulum dui eget ultrices.</p>
-</div>
-</div><div class="row gy-4 row-cols-1 row-cols-md-3 row-cols-xl-3">
-<div class="col">
-<div class="card">
-<div class="card-body p-4">
-<div class="bs-icon-md bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center d-inline-block mb-3 bs-icon"><svg class="bi bi-bell" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
-<path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"></path>
-</svg></div>
-<h4 class="card-title">Title</h4>
-<p class="card-text">Erat netus est hendrerit, nullam et quis ad cras porttitor iaculis. Bibendum vulputate cras aenean.</p>
-</div>
-</div>
-</div>
-<div class="col">
-<div class="card">
-<div class="card-body p-4">
-<div class="bs-icon-md bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center d-inline-block mb-3 bs-icon"><svg class="bi bi-bezier" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
-<path fill-rule="evenodd" d="M0 10.5A1.5 1.5 0 0 1 1.5 9h1A1.5 1.5 0 0 1 4 10.5v1A1.5 1.5 0 0 1 2.5 13h-1A1.5 1.5 0 0 1 0 11.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm10.5.5A1.5 1.5 0 0 1 13.5 9h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM6 4.5A1.5 1.5 0 0 1 7.5 3h1A1.5 1.5 0 0 1 10 4.5v1A1.5 1.5 0 0 1 8.5 7h-1A1.5 1.5 0 0 1 6 5.5v-1zM7.5 4a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z"></path>
-<path d="M6 4.5H1.866a1 1 0 1 0 0 1h2.668A6.517 6.517 0 0 0 1.814 9H2.5c.123 0 .244.015.358.043a5.517 5.517 0 0 1 3.185-3.185A1.503 1.503 0 0 1 6 5.5v-1zm3.957 1.358A1.5 1.5 0 0 0 10 5.5v-1h4.134a1 1 0 1 1 0 1h-2.668a6.517 6.517 0 0 1 2.72 3.5H13.5c-.123 0-.243.015-.358.043a5.517 5.517 0 0 0-3.185-3.185z"></path>
-</svg></div>
-<h4 class="card-title">Title</h4>
-<p class="card-text">Erat netus est hendrerit, nullam et quis ad cras porttitor iaculis. Bibendum vulputate cras aenean.</p>
-</div>
-</div>
-</div>
-<div class="col">
-<div class="card">
-<div class="card-body p-4">
-<div class="bs-icon-md bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center d-inline-block mb-3 bs-icon"><svg class="bi bi-flag" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
-<path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12.435 12.435 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A19.626 19.626 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a19.587 19.587 0 0 0 1.349-.476l.019-.007.004-.002h.001M14 1.221c-.22.078-.48.167-.766.255-.81.252-1.872.523-2.734.523-.886 0-1.592-.286-2.203-.534l-.008-.003C7.662 1.21 7.139 1 6.5 1c-.669 0-1.606.229-2.415.478A21.294 21.294 0 0 0 3 1.845v6.433c.22-.078.48-.167.766-.255C4.576 7.77 5.638 7.5 6.5 7.5c.847 0 1.548.28 2.158.525l.028.01C9.32 8.29 9.86 8.5 10.5 8.5c.668 0 1.606-.229 2.415-.478A21.317 21.317 0 0 0 14 7.655V1.222z"></path>
-</svg></div>
-<h4 class="card-title">Title</h4>
-<p class="card-text">Erat netus est hendrerit, nullam et quis ad cras porttitor iaculis. Bibendum vulputate cras aenean.</p>
-</div>
-</div>
-</div>
-</div>
-</div>
-<script src="/assets/bootstrap/js/bootstrap.min.js"></script>
-<script id="bs-live-reload" data-sseport="50601" data-lastchange="1714307386233" src="/js/livereload.js"></script>
-</body>
+<nav class="navbar">
 
+<button class="btn nav-link" onclick="showScreen('screen1')">Home <i class="bi bi-house-door"></i></button>
+<button class="btn nav-link" onclick="showScreen('screen2')">Tables <i class="bi bi-table"></i></button>
+<button class="btn nav-link" onclick="showScreen('screen3')">Contact <i class="bi bi-telephone"></i></button>
+</nav>
+<div class="container">
+<div class="content" id="screen1">
+
+
+<div class="container-fluid px-4 py-5 my-5 text-center">
+<div class="lc-block d-block mx-auto mb-4">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" width="5em" height="5em" lc-helper="svg-icon" fill="currentColor">
+<path d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm121.8 169.9l-40.7 191.8c-3 13.6-11.1 16.9-22.4 10.5l-62-45.7-29.9 28.8c-3.3 3.3-6.1 6.1-12.5 6.1l4.4-63.1 114.9-103.8c5-4.4-1.1-6.9-7.7-2.5l-142 89.4-61.2-19.1c-13.3-4.2-13.6-13.3 2.8-19.7l239.1-92.2c11.1-4 20.8 2.7 17.2 19.5z"></path>
+</svg>
+</div>
+<div class="lc-block">
+<div editable="rich">
+
+<h2 class="display-5 fw-bold">The quick brown fox jumps over the lazy dog</h2>
+
+</div>
+</div>
+<div class="lc-block col-lg-6 mx-auto mb-4">
+<div editable="rich">
+
+<p class="lead ">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+
+</div>
+</div>
+
+<div class="lc-block d-grid gap-2 d-sm-flex justify-content-sm-center"> <a class="btn btn-primary btn-lg px-4 gap-3" href="tables.html" role="button">Click me, I'm a button</a>
+<a class="btn btn-outline-secondary btn-lg px-4" href="#" role="button">Click me, I'm a button</a>
+</div>
+</div>
+
+</div>
+<div class="content" id="screen2" style="display: none;">
+
+<div class="container">
+<div class="row mb-4 align-items-center">
+<div class="col-lg-6 mb-4 mb-lg-0">
+<div class="lc-block text-center">
+<img class="img-fluid " src="https://cdn.livecanvas.com/media/svg/isometric/app_development_SVG.svg" width="400" height="400" loading="lazy">
+</div><!-- /lc-block -->
+</div><!-- /col -->
+<div class="col-lg-6 p-lg-6">
+<div class="lc-block mb-5">
+<div editable="rich">
+
+<h2 class="display-6 fw-bold">Boost your Creativity</h2>
+
+<p class="lead"><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a lacus est. Etiam diam metus.</p>
+</div>
+</div><!-- /lc-block -->
+<!-- /lc-block -->
+<div class="lc-block">
+<div class="d-inline-flex">
+<div>
+<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="text-primary" viewBox="0 0 16 16" lc-helper="svg-icon">
+<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"></path>
+</svg>
+</div>
+
+<div class="ms-3 align-self-center" editable="rich">
+<p>Lorem ipsum dolor sit amet</p>
+</div>
+</div>
+</div>
+<div class="lc-block">
+<div class="d-inline-flex">
+<div>
+<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="text-primary" viewBox="0 0 16 16" lc-helper="svg-icon">
+<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"></path>
+</svg>
+</div>
+
+<div class="ms-3 align-self-center" editable="rich">
+<p>Consectetur adipiscing elit.</p>
+</div>
+</div>
+</div>
+<div class="lc-block">
+<div class="d-inline-flex">
+<div>
+<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="text-primary" viewBox="0 0 16 16" lc-helper="svg-icon">
+<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"></path>
+</svg>
+</div>
+
+<div class="ms-3 align-self-center" editable="rich">
+<p>Aenean vel nisi in ipsum congue</p>
+</div>
+</div>
+</div><!-- /lc-block -->
+</div><!-- /col -->
+</div>
+<div class="row mb-4 align-items-center">
+<div class="col-lg-6 mb-4 mb-lg-0 order-lg-1">
+<div class="lc-block text-center"><img class="img-fluid" src="https://cdn.livecanvas.com/media/svg/isometric/Startup_SVG.svg" width="400" height="400" loading="lazy"></div><!-- /lc-block -->
+</div><!-- /col -->
+<div class="col-lg-6 p-lg-6">
+<div class="lc-block mb-5">
+<div editable="rich">
+
+<h2 class="display-6 fw-bold">Increase your Sales</h2>
+
+<p class="lead"><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a lacus est. Etiam diam metus.</p>
+</div>
+</div><!-- /lc-block -->
+<!-- /lc-block -->
+<div class="lc-block">
+<div class="d-inline-flex">
+<div>
+<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="text-primary" viewBox="0 0 16 16" lc-helper="svg-icon">
+<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"></path>
+</svg>
+</div>
+
+<div class="ms-3 align-self-center" editable="rich">
+<p>Lorem ipsum dolor sit amet</p>
+</div>
+</div>
+</div>
+<div class="lc-block">
+<div class="d-inline-flex">
+<div>
+<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="text-primary" viewBox="0 0 16 16" lc-helper="svg-icon">
+<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"></path>
+</svg>
+</div>
+
+<div class="ms-3 align-self-center" editable="rich">
+<p>Consectetur adipiscing elit.</p>
+</div>
+</div>
+</div>
+<div class="lc-block">
+<div class="d-inline-flex">
+<div>
+<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="text-primary" viewBox="0 0 16 16" lc-helper="svg-icon">
+<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"></path>
+</svg>
+</div>
+
+<div class="ms-3 align-self-center" editable="rich">
+<p>Aenean vel nisi in ipsum congue</p>
+</div>
+</div>
+</div><!-- /lc-block -->
+</div><!-- /col -->
+</div>
+
+</div>
+</div>
+<div class="content" id="screen3" style="display: none;">
+<div class="container py-5">
+<div class="row mb-5">
+<div class="col-md-12">
+<div class="lc-block text-center">
+<div editable="rich">
+<h2 class="fw-bold">FAQ</h2>
+</div>
+</div>
+<div class="lc-block text-center">
+<div editable="rich">
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu<br>fugiat nulla pariatur excepteur sint occaecat.&nbsp;<br>Static Faq<br>
+</div>
+</div>
+</div>
+</div>
+<div class="row">
+<div class="col-md-6">
+<div class="lc-block mb-5">
+<div editable="rich">
+<p class="h4">Excepteur sint occaecat cupidatat non? </p>
+<p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+</div>
+</div>
+<div class="lc-block mb-5">
+<div editable="rich">
+<p class="h4">Duis aute irure dolor in?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+</div>
+</div>
+<div class="lc-block mb-5">
+<div editable="rich">
+<p class="h4">Consequuntur magni dolores eos qui?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+</div>
+</div>
+</div>
+<div class="col-md-6">
+<div class="lc-block mb-5">
+<div editable="rich">
+<p class="h4">Duis aute irure dolor in?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+</div>
+</div>
+<div class="lc-block mb-5">
+<div editable="rich">
+<p class="h4">Consequuntur magni dolores eos qui?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+</div>
+</div>
+<div class="lc-block mb-5">
+<div editable="rich">
+<p class="h4">Excepteur sint occaecat cupidatat non? </p>
+<p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</div>
+</div>
+
+<script>
+function showScreen(screenId) {
+document.querySelectorAll('.content').forEach(content => {
+content.style.display = 'none';
+});
+document.getElementById(screenId).style.display = 'block';
+}
+</script>
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
 </html>
 """,unsafe_allow_html=True)
