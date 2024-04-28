c1b21a8b084fe4cab3c64d7e5c0f9546fb22893f
brylapl <92885606+brylapl@users.noreply.github.com>
2024-04-28 14:37:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ headers.update({
 st.set_page_config(page_title='Testowe', page_icon=":soccer:", layout="wide", initial_sidebar_state="collapsed", menu_items=None)
 #------------------------------------------------------------------------------------------------
 #STYLE CSS
-#with open('html/style.css') as f:
-    #st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
 #------------------------------------------------------------------------------------------------
 #HTML
 #with open("html/index.html", "r") as file:
