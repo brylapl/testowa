f57919345d4eabf3a1fbde5928491f9293a1a9d9
brylapl <92885606+brylapl@users.noreply.github.com>
2024-05-27 21:42:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import streamlit as st
 import undetected_chromedriver as uc
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import StaleElementReferenceException
+
 driver = uc.Chrome(headless=True,use_subprocess=False)
 url = st.text_input('Wpisz adres') 
 
