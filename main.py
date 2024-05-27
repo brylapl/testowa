a0b3156c958cf766ad626f458d41298a94fe63a9
brylapl <92885606+brylapl@users.noreply.github.com>
2024-05-27 21:59:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import undetected_chromedriver as uc
- 
+import streamlit as st
+import streamlit.components.v1 as components
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 # Define a custom user agent
 my_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"
  
