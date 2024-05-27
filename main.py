8ed2d150569a6a0bd24fa403f1700fa809ab7881
brylapl <92885606+brylapl@users.noreply.github.com>
2024-05-27 21:38:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import undetected_chromedriver as uc
 driver = uc.Chrome(headless=True,use_subprocess=False)
 url = st.text_input('Wpisz adres') 
 
-if url not == '' and st.button('Uruchom'):
+if url != '' and st.button('Uruchom'):
     driver.get(url)
     st.write(driver.title)
