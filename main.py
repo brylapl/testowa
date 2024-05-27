82da3412dca294e95320bf778d4853552394192e
brylapl <92885606+brylapl@users.noreply.github.com>
2024-05-27 21:49:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import StaleElementReferenceException
 
-from pyvirtualdisplay import Display
-display = Display(visible=0, size=(800, 800))  
-display.start()
 
-driver = uc.Chrome(headless=True,use_subprocess=False)
-url = st.text_input('Wpisz adres') 
 
-if url != '' and st.button('Uruchom'):
-    driver.get(url)
-    st.write(driver.title)
+driver = uc.Chrome() 
+driver.get('https://www.google.com') 
+st.write(driver.title)
+
+driver.quit()
