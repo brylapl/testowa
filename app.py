dd100fe9def3be29aa0e3b2d726bed58ec1a5ceb
brylapl <92885606+brylapl@users.noreply.github.com>
2024-05-30 10:53:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ driver = webdriver.Chrome(options=options)
 url = st.text_input("podaj adres strony")
 
 btn = st.button("Start")
-if btn
+if btn:
     driver.get(url)
     st.write(driver.title)
 driver.quit()
