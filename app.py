ebf57ec0eb7eaff954fdfc30ab91147354dd7a25
brylapl <92885606+brylapl@users.noreply.github.com>
2024-05-30 10:48:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-options = Options()
-options.add_argument("no-sandbox")
-options.add_argument("headless")
-options.add_argument("start-maximized")
-options.add_argument("window-size=1900,1080"); 
+options = ChromeOptions()
+options.headless = True
 driver = webdriver.Chrome(options=options)
-
-driver.get("https://www.onet.pl")
-html = driver.page_source
-st.write(html)
+driver.get('http://selenium.dev')
+st.write(driver.title)
+driver.quit()
