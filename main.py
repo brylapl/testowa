1fd7106893f7316e03599891408ea9bf321ad4e2
brylapl <92885606+brylapl@users.noreply.github.com>
2024-05-30 10:34:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import streamlit as st
 
 st.text('Test')
+
+browser = webdriver.Firefox()
+
+browser.get('http://www.yahoo.com')
+assert 'Yahoo' in browser.title
+
+elem = browser.find_element(By.NAME, 'p')  # Find the search box
+elem.send_keys('seleniumhq' + Keys.RETURN)
+st.title(browser.title)
+browser.quit()
