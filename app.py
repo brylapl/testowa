a0b9f86e0af02cfd9321ef6d4b2d735dc338ce42
brylapl <92885606+brylapl@users.noreply.github.com>
2024-06-02 14:42:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ if start:
     
     driver.switch_to.window(driver.window_handles[1])
     driver.save_screenshot("testowa/image.png")
+    st.image("testowa/image.png")
     dropdown = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '(//*[@id="sidebar-scroll"]/div/div[1]/ul/li[4]/button)[1]')))
     
     if dropdown.text.strip() == 'Last 5 games':
