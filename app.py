e3fa71977de91375aa84d0ee7d951c45319f56d2
brylapl <92885606+brylapl@users.noreply.github.com>
2024-06-02 14:28:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import streamlit as st 
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
 options = Options()
