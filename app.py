5d0b030713200db2e7b1793dae6eb7dfd8e97abb
brylapl <92885606+brylapl@users.noreply.github.com>
2024-06-02 14:46:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ from time import sleep
 
 options = Options()
 options.add_argument("--headless=new")
+options.add_argument('--verbose')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-gpu')
+options.add_argument("--window-size=1920x1080")
+options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36')
 driver = webdriver.Chrome(options=options)
 url = st.text_input("podaj adres strony")
 
