dde7a94cfb73732262ae4a8c8feddddc7a7cbfdf
brylapl <92885606+brylapl@users.noreply.github.com>
2024-07-18 12:09:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ driver = webdriver.Chrome(options=options)
 
 
 
-st.page_link("your_app.py", label="Home", icon="🏠")
-st.page_link("pages/page_1.py", label="Page 1", icon="1️⃣")
-st.page_link("pages/page_2.py", label="Page 2", icon="2️⃣", disabled=True)
-st.page_link("http://www.google.com", label="Google", icon="🌎")
+st.page_link("app.py", label="Home", icon="🏠")
+st.page_link("contact.py", label="Page 1", icon="1️⃣")
+
