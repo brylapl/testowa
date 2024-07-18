39c488faf60861116b1440c729fe06813754af8b
brylapl <92885606+brylapl@users.noreply.github.com>
2024-07-18 12:55:40 +0200

    Update ml.py

--- pages/ml.py
+++ pages/ml.py
@@ -4,3 +4,7 @@ import streamlit.components.v1 as components
 st.title('STRONA ZAWIERA ML')
 
 components.iframe("https://www.flashscore.pl", height=500, scrolling=True)
+
+components.html(
+    "<p><span style='text-decoration: line-through double red;'>Oops</span>!</p>"
+)
