430c06e8b3c50f5ea81e97546e82b7aba65fb1cf
brylapl <92885606+brylapl@users.noreply.github.com>
2024-07-18 12:53:38 +0200

    Update ml.py

--- pages/ml.py
+++ pages/ml.py
@@ -1,17 +1,5 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
+components.iframe("https://example.com", height=500)
 st.title('STRONA ZAWIERA ML')
-
-tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
-
-with tab1:
-   st.header("A cat")
-   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
-
-with tab2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-
-with tab3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
