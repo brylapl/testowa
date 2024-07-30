872671c589ec3add1c68f889d59cae4159da3754
brylapl <92885606+brylapl@users.noreply.github.com>
2024-07-30 13:30:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
     
 # options = Options()
 # options.add_argument("--headless=new")
