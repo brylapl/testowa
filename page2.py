c1403c0f9750a754633940294fd1eaadb67dfff8
brylapl <92885606+brylapl@users.noreply.github.com>
2024-07-31 14:22:35 +0200

    Create page2.py

---
+++ page2.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Strona 2")
+
+# Opcjonalnie link do powrotu do strony głównej
+if st.button("Powrót do głównej"):
+    st.experimental_rerun()  # Odświeżenie widoku
+    st.session_state.page = 'main'
