e665c9c4fdae2e4a1c542050f088bbc25c5aac58
brylapl <92885606+brylapl@users.noreply.github.com>
2024-07-31 14:07:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,37 +10,59 @@ import streamlit.components.v1 as components
 
 
 html_code = """
+<!doctype html>
+<html lang="en">
+
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Hello World</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> -->
+    <!--  <link rel="stylesheet" id="picostrap-styles-css" href="https://cdn.livecanvas.com/media/css/library/bundle.css" media="all"> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/livecanvas-team/ninjabootstrap/dist/css/bootstrap.min.css" media="all">
+
 </head>
+
 <body>
 
-<div class="container-fluid">
-<h1>Multiple DIV Elements</h1>
 
-<div class="bg-success p-5">
-  <h2>London</h2>
-  <p>London is the capital city of England.</p>
-  <p>London has over 13 million inhabitants.</p>
-</div>
+    <div class="container-fluid px-4 py-5 my-5 text-center">
+        <div class="lc-block d-block mx-auto mb-4">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" width="5em" height="5em" lc-helper="svg-icon" fill="currentColor">
+                <path d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm121.8 169.9l-40.7 191.8c-3 13.6-11.1 16.9-22.4 10.5l-62-45.7-29.9 28.8c-3.3 3.3-6.1 6.1-12.5 6.1l4.4-63.1 114.9-103.8c5-4.4-1.1-6.9-7.7-2.5l-142 89.4-61.2-19.1c-13.3-4.2-13.6-13.3 2.8-19.7l239.1-92.2c11.1-4 20.8 2.7 17.2 19.5z"></path>
+            </svg>
+        </div>
+        <div class="lc-block">
+            <div editable="rich">
 
-<div style="background-color:#FFC0C7;">
-  <h2>Oslo</h2>
-  <p>Oslo is the capital city of Norway.</p>
-  <p>Oslo has over 600.000 inhabitants.</p>
-</div>
+                <h2 class="display-5 fw-bold">The quick brown fox jumps over the lazy dog</h2>
 
-</div>
+            </div>
+        </div>
+        <div class="lc-block col-lg-6 mx-auto mb-4">
+            <div editable="rich">
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+                <p class="lead ">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+
+            </div>
+        </div>
+
+        <div class="lc-block d-grid gap-2 d-sm-flex justify-content-sm-center"> <a class="btn btn-primary btn-lg px-4 gap-3" href="#" role="button">Click me, I'm a button</a>
+            <a class="btn btn-outline-secondary btn-lg px-4" href="#" role="button">Click me, I'm a button</a>
+        </div>
+    </div>
+
+
+
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
 </body>
-</html>"""
+
+</html>
+"""
 
 components.html(html_code,height=400)
-components.iframe("https://aplikacja.streamlit.app", height=700, scrolling=True)
+
 
 # options = Options()
 # options.add_argument("--headless=new")
