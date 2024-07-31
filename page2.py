f99df0bf197b0fdcf7296596f7544a746bde11af
brylapl <92885606+brylapl@users.noreply.github.com>
2024-07-31 14:22:59 +0200

    Update page2.py

--- page2.py
+++ page2.py
@@ -5,4 +5,4 @@ st.title("Strona 2")
 # Opcjonalnie link do powrotu do strony głównej
 if st.button("Powrót do głównej"):
     st.experimental_rerun()  # Odświeżenie widoku
-    st.session_state.page = 'main'
+    st.session_state.page = 'app'
