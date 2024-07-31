3de57d9fa4685efa463ed68a88c8e0a2e9bb038e
brylapl <92885606+brylapl@users.noreply.github.com>
2024-07-31 14:36:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,45 +9,7 @@ from flask import Flask, render_template
 import streamlit.components.v1 as components
 
 # Przykładowy HTML
-html_content = '''
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script>
-        function resizeIframe(obj) {
-            obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
-        }
-    </script>
-</head>
-<body>
-<!-- Hero 6 - Bootstrap Brain Component -->
-<section class="py-4 py-md-5">
-  <div class="container">
-    <div class="row">
-      <div class="col-12">
-        <div class="container-fluid bsb-hero-6 bsb-overlay border border-dark" style="--bsb-overlay-opacity: 0.5; --bsb-overlay-bg-color: var(--bs-light-rgb); background-image: url('./assets/img/hero-img-1.webp');">
-          <div class="row justify-content-md-center align-items-center">
-            <div class="col-12 col-md-11 col-xl-10">
-              <h2 class="display-1 text-center text-md-start text-shadow-head fw-bold mb-4">Welcome to Presta</h2>
-              <p class="lead text-center text-md-start text-shadow-body mb-5 d-flex justify-content-sm-center justify-content-md-start">
-                <span class="col-12 col-sm-10 col-md-8 col-xxl-7">Where every squeak, every rattle, and every wobble finds its solution, ensuring your ride is always smooth and worry-free.</span>
-              </p>
-              <div class="d-grid gap-2 d-sm-flex justify-content-sm-center justify-content-md-start">
-                <a href="#!" class="btn bsb-btn-2xl btn-outline-dark">Explore More</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-</body>
-</html>
-'''
+
 
 # Wyświetlanie HTML w Streamlit
 st.components.v1.html(html_content, height=5000)
